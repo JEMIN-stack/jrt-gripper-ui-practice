964345ba6f63cbfe4b43544b59f197b3fb5de05a
jemin <skyjejemin@gmail.com>
2026-08-18 17:29:17 +0900

    Add mock JRT command response handling

--- mock_jrt_daemon.js
+++ mock_jrt_daemon.js
@@ -1,3 +1,4 @@
+// 실제 외부 JRT제어 프로그램 흉내
 class MockJrtDaemon {
     constructor() {
         // Daemon 프로세스가 실제로 동작중인지 나타내는 값
@@ -14,33 +15,57 @@ class MockJrtDaemon {
         console.log("[JRT Daemon] Process stopped")
     }
 
+    // -1 = Daemon not running
+    // -2 = Unknown command
     executeCommand(command) {
         if (!this.running){
             console.log("[JRT Daemon] Process need to start!")
-            return;
+            return {
+                success: false,
+                errorCode: -1
+            };
         }
 
         if (command === "OPEN"){
             console.log("[JRT Daemon] Open command received");
+
+            return {
+                success: true,
+                errorCode: 0
+            };
         }
 
-        
         else if (command === "GRIP"){
             console.log("[JRT Daemon] GRIP command received");
+
+            return {
+                success: true,
+                errorCode: 0
+            };
         }
 
         
         else if (command === "STOP"){
             console.log("[JRT Daemon] STOP command received");
+        
+            return {
+                success: true,
+                errorCode: 0
+            };
         }
 
         else{
             console.log("[JRT Daemon] Unknown command");
-            return;
+            return{
+                success: false,
+                errorCode: -2
+            };
         }
     }
 }
 
+
+// Daemon 프로세스 실행,종료,상태 제어
 class MockDaemonController{
     constructor(jrtDaemon) {
         this.jrtDaemon = jrtDaemon;
@@ -60,13 +85,14 @@ class MockDaemonController{
 
 }
 
+// 실행 중인 JRT Daemon에 명령 전달
 class MockJrtClient{
     constructor(jrtDaemon){
         this.jrtDaemon = jrtDaemon;
     }
 
     sendCommand(command) {
-        this.jrtDaemon.executeCommand(command);
+        return this.jrtDaemon.executeCommand(command);
     }
 }
 
@@ -97,6 +123,8 @@ const jrtClient = new MockJrtClient(jrtDaemon);
 // jrtClient.sendCommand("OPEN");
 // jrtClient.sendCommand("GRIP");
 
+
+// DaemonController을 외부 Contribution에 제공
 class MockDaemonService {
     constructor(daemonController){
     this.daemonController = daemonController;
@@ -113,26 +141,57 @@ class MockDaemonService {
 
 const daemonService = new MockDaemonService(controller);
 
-daemonService.getDaemon().start();
+// daemonService.getDaemon().start();
 
-console.log(daemonService.getDaemon().getState());
+// console.log(daemonService.getDaemon().getState());
 
-daemonService.getDaemon().stop();
+// daemonService.getDaemon().stop();
 
-console.log(daemonService.getDaemon().getState());
-
-
+// console.log(daemonService.getDaemon().getState());
 
 
+// UI/Plug-in 로직 입장에서 위 기능들을 조합
+class MockGripperContribution {
+    constructor(daemonService, jrtClient) {
+        this.daemonService = daemonService;        
+        this.jrtClient = jrtClient;
+    }
+
+    startDaemon() {
+        this.daemonService.getDaemon().start();
+    }
+
+    stopDaemon() {
+        this.daemonService.getDaemon().stop();
+    }
+
+    open() {
+        this.jrtClient.sendCommand("OPEN");
+    }
+
+    grip() {
+        this.jrtClient.sendCommand("GRIP");
+    }
+
+    stopMotion() {
+
+        this.jrtClient.sendCommand("STOP"); 
+    }
+
+}
 
 
+const contribution = new MockGripperContribution(daemonService, jrtClient);
 
+contribution.open();
 
+contribution.startDaemon();
 
+contribution.open();
+contribution.grip();
+contribution.stopMotion();
 
-
-
-
+contribution.stopDaemon();
 
 
 
